268687008e969e8ea0aa100153e0f5ea334053cd
MaxOliveira <maxwell_oliveira2@hotmail.com>
2026-04-19 14:04:31 -0300

    Ingestão de Dados e IO Básico

--- zenon-fraud-detector/src/main/java/br/com/zenon/Main.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/Main.java
@@ -1,18 +1,38 @@
 package br.com.zenon;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    static void main() {
-        Transaction transaction1 = new Transaction(1, TransactionType.PAYMENT.name(),  new BigDecimal("9839.64"), "C1231006815",
-                new BigDecimal("170136.0"),  new BigDecimal("160296.36"), "M1979787155",
-                new BigDecimal("0.0"), new BigDecimal("0.0"), 0, 0);
+    static void main() throws Exception{
+       List<Transaction> transactions = new ArrayList<>();
+       try (BufferedReader br = new BufferedReader(new FileReader("../zenon-fraud-detector/data/PS_20174392719_1491204439457_log.csv"))) {
+           br.readLine();
+           int linesCount = 0;
+           String line;
+            while ((line = br.readLine()) != null && linesCount < 10) {
+                String[] fields = line.split(",");
 
-        Transaction transaction2 = new Transaction(743, TransactionType.CASH_OUT.name(),  new BigDecimal("850002.52"), "C1280323807",
-                new BigDecimal("850002.52"),  new BigDecimal("0.0"), "C873221189",
-                new BigDecimal("6510099.11"), new BigDecimal("7360101.63"), 1, 0);
-
-        System.out.println("Transação 1: " + transaction1);
-        System.out.println("Transação 2: " + transaction2);
+                Transaction transaction = new Transaction(
+                        Integer.parseInt(fields[0]),        // step
+                        fields[1],                          // type
+                        new BigDecimal(fields[2]),          // amount
+                        fields[3],                          // nameOrig
+                        new BigDecimal(fields[4]),          // oldbalanceOrg
+                        new BigDecimal(fields[5]),          // newbalanceOrig
+                        fields[6],                          // nameDest
+                        new BigDecimal(fields[7]),          // oldbalanceDest
+                        new BigDecimal(fields[8]),          // newbalanceDest
+                        Integer.parseInt(fields[9]),        // isFraud
+                        Integer.parseInt(fields[10])        // isFlaggedFraud
+                );
+                transactions.add(transaction);
+                linesCount++;
+            }
+            transactions.forEach(System.out::println);
+        }
     }
 }
